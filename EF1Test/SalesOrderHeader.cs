1dfdb1e2c211cca60bcf05792ada3716d4b7fa10
Davy Landman <davy.landman@mumc.nl>
2010-08-23 16:01:22 +0200

    Added the mapping info to the SalesOrderHeader and fixed the enum values

--- EF1Test/SalesOrderHeader.cs
+++ EF1Test/SalesOrderHeader.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace azM.EFWRappableFields.EF1Tests
 {
 	public enum OrderState
 	{
 		Unknown = 0,
-		Initial = 1,
-		WaitingForPayment = 2,
-		PaymentRecieved = 3,
-		Packaging = 4,
-		Shipping = 5,
-		Recieved = 6
+		InProcess = 1,
+		Approved = 2,
+		Backordered = 3,
+		Rejected = 4,
+		Shipped = 5,
+		Cancelled = 6
 	}
 
-	public partial class SalesOrderHeader
+	public partial class SalesOrderHeader :IHaveFieldsWrapped
 	{
 		public OrderState Status
 		{
 			get { return (OrderState)DbStatus; }
 			set { DbStatus = (byte)value; }
+		}
+
+		public Dictionary<MemberInfo, MemberInfo> GetMappings()
+		{
+			return new Dictionary<MemberInfo, MemberInfo> { { ExtractMemberInfo.From<SalesOrderHeader>(s => s.Status), ExtractMemberInfo.From<SalesOrderHeader>(s => s.DbStatus) } };
 		}
 	}
 }
