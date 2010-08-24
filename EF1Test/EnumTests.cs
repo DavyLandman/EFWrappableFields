81da64cdff610ed5a94f90ce9eeef8d7161b1ce0
Davy Landman <davy.landman@mumc.nl>
2010-08-24 13:42:14 +0200

    Added EF4 support, compiling and testing for EF1 and EF4

--- EF1Test/EnumTests.cs
+++ EF1Test/EnumTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Xunit;
 
-namespace EFExtensions.EFWRappableFields.EF1Tests
+namespace EFExtensions.EFWRappableFields.EFTests
 {
 	public class EnumTests
 	{
